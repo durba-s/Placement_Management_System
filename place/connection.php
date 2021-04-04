bd5850cc103c273134f63167f84d29384cfcf749
Durba Satpathi <70879852+durba-s@users.noreply.github.com>
2021-04-04 23:32:25 +0530

    Uploaded place

--- place/connection.php
+++ place/connection.php
@@ -4,9 +4,6 @@ $dbhost="localhost";
 $dbuser="root";
 $dbpass="";
 $dbname="trial";
-//$pdo = new PDO('mysql:host=localhost;dbname=trial', 'root', '');
-//$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 if(!$con=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
 	die("failed to connect!");
